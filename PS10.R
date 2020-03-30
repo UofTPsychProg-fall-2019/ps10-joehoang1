5fd6bab39a69cf9b7bd4aa9687c4e0095f9b466a
Joe Hoang <joe.hoang@mail.utoronto.ca>
2020-03-30 04:19:17 -0400

    Final Submission
    (:

--- PS10.R
+++ PS10.R
@@ -4,6 +4,8 @@ library(Hmisc)
 # This problem set will test out your ggploting skills using the Big 5 health dataset 
 # that you wrangled in problem set 9
 
+#Set Working Directory
+setwd("~/Documents/GitHub/ps10-joehoang1")
 # load in the data
 ipip <- read_csv('ipip50_sample.csv')
 
@@ -57,7 +59,10 @@ ipip.l <- ipip %>%
 # create a boxplot that visualizes BMI distributions according to exercise habits, separately for females and males
 # include at least two customizations to the look of the boxplot 
 # check the documentation for options
-Q1 <- ggplot()
+Q1 <- ggplot(ipip, aes(x=exer, y=BMI, color=gender)) +
+      geom_violin(aes(color = gender), trim = FALSE, position = position_dodge(0.9) ) +
+      geom_boxplot(aes(color = gender), width = 0.15, position = position_dodge(0.9)) +
+      theme_classic()
 Q1
 ggsave('figures/Q1.pdf',units='in',width=4,height=5)
 
@@ -67,13 +72,17 @@ ggsave('figures/Q1.pdf',units='in',width=4,height=5)
 # use geom_smooth to add linear model fit lines, separately for males and females
 Q2a <- ggplot(ipip,aes(x=logMedInc,y=BMI, color=gender))+
     geom_point(size=.5,alpha=.4)+
-    geom_smooth(method='lm')
+    geom_smooth(method='lm') +
+    theme_bw()
 Q2a
 ggsave('figures/Q2a.pdf',units='in',width=4,height=5)
 
 # there are some outlying lower income points, especially for females
 # recreate this graph filtering for log median income>10
-Q2b <- ggplot()
+Q2b <- ggplot(ipip %>% filter(logMedInc>10),aes(x=logMedInc,y=BMI, color=gender))+
+  geom_point(size=.5,alpha=.4)+
+  geom_smooth(method='lm')+ 
+  theme_bw()
 Q2b
 ggsave('figures/Q2b.pdf',units='in',width=4,height=5)
 
@@ -85,7 +94,11 @@ ggsave('figures/Q2b.pdf',units='in',width=4,height=5)
 # the default range on the y-axis will be very large given the range of the data
 # add a +coord_cartesian(ylim = c(10, 12)) to rescale it.
 
-Q3 <- ggplot()
+Q3 <- ggplot(ipip, aes(x=gender, y=logMedInc, color=exer)) +  
+      stat_summary(geom = "bar", fun.y = mean, position = "dodge", fill=aes(exer)) + 
+      coord_cartesian(ylim = c(10, 12)) +
+      stat_summary(geom = "errorbar", fun.data = mean_cl_boot, position ="dodge", size=.2, width=.9)+ 
+      theme_light()
 Q3
 ggsave('figures/Q3.pdf',units='in',width=4,height=5)
 
@@ -95,21 +108,39 @@ ggsave('figures/Q3.pdf',units='in',width=4,height=5)
 # for each BMI category, separately for males and females
 # this is a lot to visualize in a single plot! use +facet_wrap(vars(trait)) to generate seperate plots for each personality trait
 
-Q4 <- ggplot()
+Q4 <- ggplot(ipip.l, aes(x=BMI_cat, y=value, group=gender, color =gender)) +  
+      facet_wrap(vars(trait)) +
+      stat_summary(fun.data=mean_cl_boot, geom="pointrange") +
+      theme_minimal()
 Q4
 ggsave('figures/Q4.pdf',units='in',width=4,height=5)
 
 
 # Q5 re-visualizing gender differences in personality as function of BMI  ---------------------------------------
 
-# use dplyr functions to calculate the mean of each personality trait for each combination of gender, BMI group
-ipip.g <- ipip.l %>%
-    ...
+#library(plyr)
 
-
+#ipip.g <- ipip.l %>%  ddply(c("trait", "gender", "BMI_cat"), summarise,
+                            value=mean(value))
 # plot the average value of personality trait (colored as separate lines), according to the BMI category
 # facet_warp gender so that you can see these relationships separately for females and males
-Q5 <- ggplot()
+#Q5 <- ggplot(ipip.g, aes(x=BMI_cat, y=value, group=trait, colour=trait)) +
+      geom_line() +
+      geom_point() +
+      facet_wrap(vars(gender)) + 
+      theme_minimal()
+#Q5
+ggsave('figures/Q5.pdf',units='in',width=4,height=5)
+
+# use dplyr functions to calculate the mean of each personality trait for each combination of gender, BMI group
+ipip.g <- ipip.l %>% group_by(trait, gender, BMI_cat) %>% summarise_each(mean)
+
+Q5 <- ggplot(ipip.g1, aes(x=BMI_cat, y=value, group=trait, colour=trait)) +
+  geom_line() +
+  geom_point() +
+  facet_wrap(vars(gender)) + 
+  theme_minimal()
 Q5
 ggsave('figures/Q5.pdf',units='in',width=4,height=5)
+
     
